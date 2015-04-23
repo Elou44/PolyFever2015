69a19f652952c5adb591412b4ad3cfc7c2e2e6af
Freddy <frederic.rollac@gmail.com>
2015-04-23 20:42:46 +0200

    Premier push Module Moteur De Jeu

---
+++ polyFever/module/moteurDeJeu/BonusEpaisseur.java
@@ -0,0 +1,12 @@
+package polyFever.module.moteurDeJeu;
+
+public class BonusEpaisseur extends Bonus{
+
+	public void modifierParametres()
+	{
+		/* Changer l'épaisseur du joueur affecté
+		 * 
+		 */
+	}
+	
+}
