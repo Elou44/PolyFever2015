2ac70f84b24f78790eb9fa6828c8f47c991577c8
unknown <elouarn.laine@gmail.com>
2015-04-24 01:32:49 +0200

    Initialisation du dépôt...

---
+++ src/polyFever/module/moteurDeJeu/BonusVitesse.java
@@ -0,0 +1,21 @@
+package polyFever.module.moteurDeJeu;
+
+public class BonusVitesse extends Bonus{
+
+	// Constructeur
+	public BonusVitesse()
+	{
+		this.nom = "vitesse";
+		this.couleur = "vert";
+		this.coordonnees = ;
+		this.joueur = 
+	}
+	
+	public void modifierParametres()
+	{
+		/* Changer la vitesse du joueur affecté
+		 * 
+		 */
+	}
+	
+}
