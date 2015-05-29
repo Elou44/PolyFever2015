f4a5f775112b0bd9ce11b6452174b339a9de6bf4
Dwidwi <hamburger_man44@hotmail.fr>
2015-05-29 23:19:13 +0200

    test

--- src/polyFever/module/menu/BoutonMenuPlay.java
+++ src/polyFever/module/menu/BoutonMenuPlay.java
@@ -1,8 +1,24 @@
 package polyFever.module.menu;
 
+/**
+ * Classe correspondant au bouton du menu qui lancera la partie
+ * @author Ambre
+ *
+ */
+
 public class BoutonMenuPlay extends Bouton {
 	protected Menu pere;
 	protected MenuPlay fils;
+	
+	/**
+	 * Constructeur du bouton asocié au menu qui lancera le jeu
+	 * @param p : Menu affiché
+	 * @param f : Menu qui lance le jeu
+	 * @param x : Coordonnée x du centre de la hitbox
+	 * @param y : Coordonnée y du centre de la hitbox
+	 * @param l : Longueur de la hitbox
+	 * @param h : Largeur de la hitbox
+	 */
 	
 	public BoutonMenuPlay(Menu p, MenuPlay f, float x, float y, float l, float h){
 		super(x, y, l, h);
