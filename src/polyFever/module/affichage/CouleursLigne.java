7aafdd98eef0d6e7f4e082d274a164364c00f9b2
elou44 <elouarn.laine@gmail.com>
2015-06-01 12:14:15 +0200

    classe CouleursLigne

---
+++ src/polyFever/module/affichage/CouleursLigne.java
@@ -0,0 +1,23 @@
+package polyFever.module.affichage;
+import polyFever.module.util.math.*;
+
+/**
+ * Classe permettant d'accéder aux différentes couleurs disponible pour les joueurs
+ * @author Elouarn Lainé
+ *
+ */
+public class CouleursLigne {
+	
+	/**
+	 * Tableau de {@link Vector3} contenant les couleurs disponibles.
+	 */
+	public static final Vector3 tabCouleurs[] = new Vector3[] {
+			
+			new Vector3(1.0f,0.0f,0.0f),
+			new Vector3(0.0f,1.0f,0.0f),
+			new Vector3(0.0f,0.0f,1.0f),
+			new Vector3(1.0f,1.0f,0.0f)
+			
+	};
+
+}
