ddab46a46369cc9f5ed502b43ef8ae17552c51da
FreddyPoly <frederic.rollac@gmail.com>
2015-06-05 13:14:41 +0200

    32 touches pour 16 joueurs

--- src/polyFever/module/menu/MenuPlay.java
+++ src/polyFever/module/menu/MenuPlay.java
@@ -51,35 +51,35 @@ public class MenuPlay extends Menu {
 				
 					if(b.getIdColor()==0) { Joueur j1 = new Joueur(partie, "Joueur 1", Keyboard.KEY_LEFT, Keyboard.KEY_RIGHT);partie.ajouterJoueur(j1, p, 0); }
 
-					else if(b.getIdColor()==1) { Joueur j2 = new Joueur(partie, "Joueur 2", Keyboard.KEY_V, Keyboard.KEY_B); partie.ajouterJoueur(j2, p, 1); }
+					else if(b.getIdColor()==1) { Joueur j2 = new Joueur(partie, "Joueur 2", Keyboard.KEY_C, Keyboard.KEY_V); partie.ajouterJoueur(j2, p, 1); }
 
 					else if(b.getIdColor()==2) { Joueur j3 = new Joueur(partie, "Joueur 3", Keyboard.KEY_A, Keyboard.KEY_Z); partie.ajouterJoueur(j3, p, 2); }
 
-					else if(b.getIdColor()==3) { Joueur j4 = new Joueur(partie, "Joueur 4", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j4, p, 3);  }
+					else if(b.getIdColor()==3) { Joueur j4 = new Joueur(partie, "Joueur 4", Keyboard.KEY_J, Keyboard.KEY_K); partie.ajouterJoueur(j4, p, 3);  }
 
-					else if(b.getIdColor()==4) { Joueur j5 = new Joueur(partie, "Joueur 5", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j5, p, 4); }
+					else if(b.getIdColor()==4) { Joueur j5 = new Joueur(partie, "Joueur 5", Keyboard.KEY_E, Keyboard.KEY_R); partie.ajouterJoueur(j5, p, 4); }
 
-					else if(b.getIdColor()==5) { Joueur j6 = new Joueur(partie, "Joueur 6", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j6, p, 5); }
+					else if(b.getIdColor()==5) { Joueur j6 = new Joueur(partie, "Joueur 6", Keyboard.KEY_T, Keyboard.KEY_Y); partie.ajouterJoueur(j6, p, 5); }
 
-					else if(b.getIdColor()==6) { Joueur j7 = new Joueur(partie, "Joueur 7", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j7, p, 6); }
+					else if(b.getIdColor()==6) { Joueur j7 = new Joueur(partie, "Joueur 7", Keyboard.KEY_U, Keyboard.KEY_I); partie.ajouterJoueur(j7, p, 6); }
 
-					else if(b.getIdColor()==7) { Joueur j8 = new Joueur(partie, "Joueur 8", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j8, p, 7); }
+					else if(b.getIdColor()==7) { Joueur j8 = new Joueur(partie, "Joueur 8", Keyboard.KEY_O, Keyboard.KEY_P); partie.ajouterJoueur(j8, p, 7); }
 
-					else if(b.getIdColor()==8) { Joueur j9 = new Joueur(partie, "Joueur 9", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j9, p, 8); }
+					else if(b.getIdColor()==8) { Joueur j9 = new Joueur(partie, "Joueur 9", Keyboard.KEY_Q, Keyboard.KEY_S); partie.ajouterJoueur(j9, p, 8); }
 
-					else if(b.getIdColor()==9) { Joueur j10 = new Joueur(partie, "Joueur 10", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j10, p, 9); }
+					else if(b.getIdColor()==9) { Joueur j10 = new Joueur(partie, "Joueur 10", Keyboard.KEY_D, Keyboard.KEY_F); partie.ajouterJoueur(j10, p, 9); }
 
-					else if(b.getIdColor()==10) { Joueur j11 = new Joueur(partie, "Joueur 11", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j11, p, 10); }
+					else if(b.getIdColor()==10) { Joueur j11 = new Joueur(partie, "Joueur 11", Keyboard.KEY_G, Keyboard.KEY_H); partie.ajouterJoueur(j11, p, 10); }
 
-					else if(b.getIdColor()==11) { Joueur j12 = new Joueur(partie, "Joueur 12", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j12, p, 11); }
+					else if(b.getIdColor()==11) { Joueur j12 = new Joueur(partie, "Joueur 12", Keyboard.KEY_L, Keyboard.KEY_M); partie.ajouterJoueur(j12, p, 11); }
 
-					else if(b.getIdColor()==12) { Joueur j13 = new Joueur(partie, "Joueur 13", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j13, p, 12); }
+					else if(b.getIdColor()==12) { Joueur j13 = new Joueur(partie, "Joueur 13", Keyboard.KEY_W, Keyboard.KEY_X); partie.ajouterJoueur(j13, p, 12); }
 
-					else if(b.getIdColor()==13) { Joueur j14 = new Joueur(partie, "Joueur 14", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j14, p, 13); }
+					else if(b.getIdColor()==13) { Joueur j14 = new Joueur(partie, "Joueur 14", Keyboard.KEY_B, Keyboard.KEY_N); partie.ajouterJoueur(j14, p, 13); }
 
-					else if(b.getIdColor()==14) { Joueur j15 = new Joueur(partie, "Joueur 15", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j15, p, 14); }
+					else if(b.getIdColor()==14) { Joueur j15 = new Joueur(partie, "Joueur 15", Keyboard.KEY_6, Keyboard.KEY_9); partie.ajouterJoueur(j15, p, 14); }
 
-					else if(b.getIdColor()==15) { Joueur j16 = new Joueur(partie, "Joueur 16", Keyboard.KEY_K, Keyboard.KEY_L); partie.ajouterJoueur(j16, p, 15); }
+					else if(b.getIdColor()==15) { Joueur j16 = new Joueur(partie, "Joueur 16", Keyboard.KEY_1, Keyboard.KEY_0); partie.ajouterJoueur(j16, p, 15); }
 
 				
 			}
